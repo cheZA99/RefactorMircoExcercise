467fb78339edc354cb232344405dd27278d598c6
zela <amerzelic08@gmail.com>
2023-07-17 22:08:13 +0200

    written unit tests for Alarm and Sensor class

---
+++ src/TirePressureMonitoringSystem.Tests/AlarmTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
+{
+    [TestFixture]
+    public class AlarmTests
+    {
+        private IAlarm _alarm;
+        private Mock<ISensor> _sensorMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sensorMock = new Mock<ISensor>();
+            _alarm = new Alarm(_sensorMock.Object);
+        }
+
+        [TestCase(16.5, true)]
+        [TestCase(2, true)]
+        [TestCase(520, true)]
+        [TestCase(17.5, false)]
+        [TestCase(20.2, false)]
+        [TestCase(19.888888, false)]
+        public void Check_Alarm_Should_Be_On_Or_Off(double pressure, bool state)
+        {
+            _sensorMock.Setup(s => s.PopNextPressurePsiValue()).Returns(pressure);
+            _alarm.Check();
+            Assert.That(state, Is.EqualTo(_alarm.AlarmOn));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //no need for disposing anything
+        }
+    }
+}
---
+++ src/TirePressureMonitoringSystem.Tests/SensorTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
+
+{
+    [TestFixture]
+    public class SensorTests
+    {
+        private ISensor _sensor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sensor = new Sensor();
+        }
+
+        [TestCase(16, 22, true)]
+        [TestCase(9, 15.9, false)]
+        [TestCase(22.1, 30, false)]
+        public void Test_Pressure_In_Range(double min, double max, bool isValid)
+        {
+            var value = _sensor.PopNextPressurePsiValue();
+            Assert.That(value >= min && value <= max, Is.EqualTo(isValid));
+        }
+    }
+}
