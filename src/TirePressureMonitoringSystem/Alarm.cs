0e3d0a5d58e1b72724efe603ab7e7d36814cb722
zela <amerzelic08@gmail.com>
2023-07-17 21:12:39 +0200

    updated Alarm and Sensor clasees, DI implemented

--- src/TirePressureMonitoringSystem/Alarm.cs
+++ src/TirePressureMonitoringSystem/Alarm.cs
@@ -1,28 +1,39 @@
 namespace TDDMicroExercises.TirePressureMonitoringSystem
 {
-    public class Alarm
+    public class Alarm : IAlarm
     {
         private const double LowPressureThreshold = 17;
         private const double HighPressureThreshold = 21;
 
-        readonly Sensor _sensor = new Sensor();
+        private readonly ISensor _sensor;
+        private bool _alarmOn = false;
 
-        bool _alarmOn = false;
+        public Alarm(ISensor sensor)
+        {
+            _sensor = sensor;
+        }
+
+        public Alarm()
+        {
+            //Personally, I wouldn't do this in real-world apps since we are relying on concrete type.
+            //But this is only because we can't change client classes.
+            //Otherwise I would inject ISensor like I did in other constructor. 
+            _sensor = new Sensor();
+        }
 
         public void Check()
         {
             double psiPressureValue = _sensor.PopNextPressurePsiValue();
-
             if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
             {
                 _alarmOn = true;
             }
+            else
+            {
+                _alarmOn = false;
+            }
         }
 
-        public bool AlarmOn
-        {
-            get { return _alarmOn; }
-        }
-
+        public bool AlarmOn => _alarmOn;
     }
 }
